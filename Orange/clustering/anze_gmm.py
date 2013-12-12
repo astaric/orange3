a55fc9eaaea5bac86e757d2d8bce4ec6ec26c54b
astaric <anze.staric@gmail.com>
2013-12-12 10:09:11 +0100

    Fix crash when one cluster lost all members.

--- Orange/clustering/anze_gmm.py
+++ Orange/clustering/anze_gmm.py
@@ -19,6 +19,7 @@ def em(X, k, nsteps=30):
     w = np.empty((k, m))
 
     for i in range(1, nsteps+1):
+        active = ones(k, dtype=np.bool)
         print("Step ", i)
         for l in range(X.shape[1]):
             dims = slice(l-1 if l > 0 else None, l+2 if l < dim - 1 else None)
@@ -27,34 +28,46 @@ def em(X, k, nsteps=30):
 
             # E step
             for j in range(k):
-                det = covars[j, dims].prod()
-                inv_covars = 1. / covars[j, dims]
-                xn = x - means[j, dims]
-                factor = (2.0 * np.pi) ** (x.shape[1] / 2.0) * det ** 0.5
-                w[j] = priors[j] * exp(-.5 * np.sum(xn * inv_covars * xn, axis=1)) / factor
-            w /= w.sum(axis=0)
+                if any(np.abs(covars[j, dims]) < 1e-15):
+                    active[j] = 0
+
+                if active[j]:
+                    det = covars[j, dims].prod()
+                    inv_covars = 1. / covars[j, dims]
+                    xn = x - means[j, dims]
+                    factor = (2.0 * np.pi) ** (x.shape[1] / 2.0) * det ** 0.5
+                    w[j] = priors[j] * exp(-.5 * np.sum(xn * inv_covars * xn, axis=1)) / factor
+                else:
+                    w[j] = 0
+            w[active] /= w[active].sum(axis=0)
 
             # M step
             n = np.sum(w, axis=1)
             priors = n / np.sum(n)
             for j in range(k):
-                mu = np.dot(w[j, :], x[:, active_dim]) / n[j]
+                if n[j]:
+                    mu = np.dot(w[j, :], x[:, active_dim]) / n[j]
 
-                xn = x[:, active_dim] - mu
-                sigma = np.sum(xn ** 2 * w[j], axis=0) / n[j]
+                    xn = x[:, active_dim] - mu
+                    sigma = np.sum(xn ** 2 * w[j], axis=0) / n[j]
 
-                if np.isnan(mu).any() or np.isnan(sigma).any():
-                    return w, means, covars, priors
+                    if np.isnan(mu).any() or np.isnan(sigma).any():
+                        return w, means, covars, priors
+                else:
+                    active[j] = 0
+                    mu = 0.
+                    sigma = 0.
                 means[j, l] = mu
                 covars[j, l] = sigma
 
     w = np.zeros((k, m))
     for j in range(k):
-        det = covars[j].prod()
-        inv_covars = 1. / covars[j]
-        xn = X - means[j]
-        factor = (2.0 * np.pi) ** (xn.shape[1] / 2.0) * det ** 0.5
-        w[j] = priors[j] * exp(-.5 * np.sum(xn * inv_covars * xn, axis=1)) / factor
-        w /= w.sum(axis=0)
+        if active[j]:
+            det = covars[j].prod()
+            inv_covars = 1. / covars[j]
+            xn = X - means[j]
+            factor = (2.0 * np.pi) ** (xn.shape[1] / 2.0) * det ** 0.5
+            w[j] = priors[j] * exp(-.5 * np.sum(xn * inv_covars * xn, axis=1)) / factor
+        w[active] /= w[active].sum(axis=0)
 
     return w, means, covars, priors
